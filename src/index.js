9a4d4130d1f7d7abef2e5d57d94762eae82bba8a
Sebastián Rizzuto <srizzuto@Sebastians-Air.fibertel.com.ar>
2021-08-25 21:10:32 -0300

    add logic and style

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+let description = document.getElementById('description');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const task = [
+  {
+    description: 'Hello',
+    completed: false,
+    index: 1
+  },
+  {
+    description: 'Everyone',
+    completed: true,
+    index: 2
+  },
+  {
+    description: 'Welcome',
+    completed: false,
+    index: 3
+  }
+]
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+function render() {
+  task.forEach(tsk => {
+    const { description, completed, index } = tsk;
+    let taskList = document.getElementById('task-list');
+    let newLi = document.createElement('li');
+    let pContainer = document.createElement('div');
+    let icon = document.createElement('i');
+    let newP = document.createElement('p');
+    let checkbox = document.createElement('input');
 
-  element.appendChild(btn);
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox";
+    icon.className = "fas fa-ellipsis-v p5";
+    newLi.className = "border-bottom just-between";
 
-  return element;
+    newP.innerHTML = description;
+
+
+    pContainer.appendChild(checkbox);
+    pContainer.appendChild(newP);
+    newLi.appendChild(pContainer);
+    newLi.appendChild(icon);
+    taskList.appendChild(newLi);
+  })
 }
 
-document.body.appendChild(component());
+render();
