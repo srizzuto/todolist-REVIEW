45e7ab4f30c7634482da5a56fe033cbbf0b2397a
Sebastián Rizzuto <srizzuto@Sebastians-Air.fibertel.com.ar>
2021-08-25 21:18:57 -0300

    fix stylint eslint

--- src/index.js
+++ src/index.js
@@ -1,50 +1,46 @@
-import _ from 'lodash';
 import './style.css';
-
-let description = document.getElementById('description');
 
 const task = [
   {
     description: 'Hello',
     completed: false,
-    index: 1
+    index: 1,
   },
   {
     description: 'Everyone',
     completed: true,
-    index: 2
+    index: 2,
   },
   {
     description: 'Welcome',
     completed: false,
-    index: 3
-  }
-]
+    index: 3,
+  },
+];
 
 function render() {
-  task.forEach(tsk => {
-    const { description, completed, index } = tsk;
-    let taskList = document.getElementById('task-list');
-    let newLi = document.createElement('li');
-    let pContainer = document.createElement('div');
-    let icon = document.createElement('i');
-    let newP = document.createElement('p');
-    let checkbox = document.createElement('input');
+  task.forEach((tsk) => {
+    const { description } = tsk;
+    const taskList = document.getElementById('task-list');
+    const newLi = document.createElement('li');
+    const pContainer = document.createElement('div');
+    const icon = document.createElement('i');
+    const newP = document.createElement('p');
+    const checkbox = document.createElement('input');
 
-    checkbox.type = "checkbox";
-    checkbox.id = "checkbox";
-    icon.className = "fas fa-ellipsis-v p5";
-    newLi.className = "border-bottom just-between";
+    checkbox.type = 'checkbox';
+    checkbox.id = 'checkbox';
+    icon.className = 'fas fa-ellipsis-v p5';
+    newLi.className = 'border-bottom just-between';
 
     newP.innerHTML = description;
-
 
     pContainer.appendChild(checkbox);
     pContainer.appendChild(newP);
     newLi.appendChild(pContainer);
     newLi.appendChild(icon);
     taskList.appendChild(newLi);
-  })
+  });
 }
 
 render();
